0e3a1cd8afef992160f5ef90b417c757f4171518
Grant <grant.kiely@gmail.com>
2016-01-11 23:52:53 +1100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// var babel         = require('gulp-babel'),
+    // bulkSass      = require('gulp-sass-bulk-import'),
+    // cache         = require('gulp-cached'),
+    // concat        = require('gulp-concat'),
+    // config        = require('./gulp/gulp-config.js'),
+    // eslint        = require('gulp-eslint'),
+    // fileInclude   = require('gulp-file-include'),
+var gulp          = require('gulp'),
+    // livereload    = require('gulp-livereload'),
+    // prefix        = require('gulp-autoprefixer'),
+    // react         = require('gulp-react'),
+    // remember      = require('gulp-remember');
+    // sass          = require('gulp-sass'),
+    // sourcemaps    = require('gulp-sourcemaps'),
+    webserver     = require('gulp-webserver'),
+
+    //Build only
+    htmlmin       = function(){},
+    minifyCSS     = function(){},
+    uglify        = function(){};
+
+
